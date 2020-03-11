d3697d037018c151855fd277748a11432d733861
Christopher Cid <iamcid17@gmail.com>
2020-03-11 05:58:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,9 @@
 def reverse_each_word(phrase)
   reverse_phrase = phrase.reverse
+  reverse_phrase_array = reverse_phrase.split
+  new_phrase = []
+  reverse_phrase_array.each do |words|
+    new_phrase.unshift(words)
+  end
+  new_phrase.join(" ")
 end
